a07b7e4ca4e708fd0e1c4113868f2f16a2b57e79
vimahemo2 <vimahemo@outlook.com>
2022-01-06 17:13:39 -0500

    Update miprimercambio.php

--- miprimercambio.php
+++ miprimercambio.php
@@ -1,10 +1,14 @@
 <html>
     <body>
         <?php
+<<<<<<< Updated upstream
 
             echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE</h1>';
 
 
+=======
+            echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE PRODUCCION </h1>';
+>>>>>>> Stashed changes
         ?>
     <body>
 </html>
