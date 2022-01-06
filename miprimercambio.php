a8840fb8c2786e717d234b268132d0acc0a7f478
vimahemo2 <vimahemo@outlook.com>
2022-01-06 14:36:10 -0500

    Update miprimercambio.php
    EJEMPLO

--- miprimercambio.php
+++ miprimercambio.php
@@ -1,7 +1,7 @@
 <html>
     <body>
         <?php
-            echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE PRUEBA</h1>';
+            echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE TESTING</h1>';
         ?>
     <body>
 </html>
