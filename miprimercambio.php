73feb062d646032438a3a1f3a42403f493618296
vimahemo2 <vimahemo@outlook.com>
2022-01-06 17:47:13 -0500

    Update miprimercambio.php
    TESTING5

--- miprimercambio.php
+++ miprimercambio.php
@@ -5,7 +5,7 @@
             echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE</h1>';
 
 
-            echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE PRODUCCION </h1>';
+            echo '<h1 style="color:red;">TESTING 5 </h1>';
         ?>
     <body>
 </html>
