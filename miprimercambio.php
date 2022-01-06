aae56f5c6871e2a82469e8e7594c4ea30b12d663
vimahemo2 <vimahemo@outlook.com>
2022-01-06 15:54:58 -0500

    Update miprimercambio.php

--- miprimercambio.php
+++ miprimercambio.php
@@ -1,13 +1,10 @@
 <html>
     <body>
         <?php
-<<<<<<< HEAD
-            echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE PROOF</h1>';
-=======
 
-            echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE PRODUCCION</h1>';
+            echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE</h1>';
 
->>>>>>> 1822f418947afa0d83cbdb59d6584f7920e476fc
+
         ?>
     <body>
 </html>
