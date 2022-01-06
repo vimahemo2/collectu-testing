79926d60969d9af52e609a83b506dc7a2f559a31
vimahemo2 <vimahemo@outlook.com>
2022-01-06 17:33:46 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <html>
     <body>
         <?php
+            echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE PRUEBA</h1></br>';
             echo '<h1 style="color:red;">ESTA ES UNA PAGINA DE PRUEBA</h1>';
         ?>
     <body>
